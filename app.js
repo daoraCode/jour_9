78668a2892a498fa3fc0e2c7bd4cbe2b0256ce7b
daoraCode <severin.mboukou@gmail.com>
2021-10-11 07:25:15 +0200

    Exercice 2, fonction des mois de l'année, terminé

--- app.js
+++ app.js
@@ -3,21 +3,16 @@ var moment = require('moment'); // import du module moment
 
 moment().format("[Aujourd'hui nous sommes] dddd");
 moment.locale('en');
-moment.locale('fr'); // use french paramter translation with moment
-
-var days = [
-  'dimanche',
-  'lundi',
-  'mardi',
-  'mercredi',
-  'jeudi',
-  'vendredi',
-  'samedi',
-];
+moment.locale('fr'); // permet un affichage en version fr
 
 // EXERCICE 1
 function whatDayIsToday() {
-  moment().format("[Aujourd'hui nous sommes] dddd");
   console.log(moment().format("[Aujourd'hui nous sommes] dddd."));
 }
 whatDayIsToday();
+
+// EXERCICE 2
+function whatMonthIsIt() {
+  console.log(moment().format("[Nous sommes au mois :] MMMM."));
+}
+whatMonthIsIt();
