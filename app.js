015922274dba050073590fc2f180962483e52f59
daoraCode <severin.mboukou@gmail.com>
2021-10-11 07:18:06 +0200

    Exercice 1, version moment-packages termniné

--- app.js
+++ app.js
@@ -1,65 +1,23 @@
-// Moment packages exercises versions, works on Date() object
+var moment = require('moment'); // import du module moment
+// moment().format();
 
+moment().format("[Aujourd'hui nous sommes] dddd");
+moment.locale('en');
+moment.locale('fr'); // use french paramter translation with moment
+
+var days = [
+  'dimanche',
+  'lundi',
+  'mardi',
+  'mercredi',
+  'jeudi',
+  'vendredi',
+  'samedi',
+];
 
 // EXERCICE 1
-
 function whatDayIsToday() {
-
+  moment().format("[Aujourd'hui nous sommes] dddd");
+  console.log(moment().format("[Aujourd'hui nous sommes] dddd."));
 }
-
-
-
-
-
-
-
-
-
-
-
-
-// EXERCICE 2
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// EXERCICE 3
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// EXERCICE 4
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+whatDayIsToday();
